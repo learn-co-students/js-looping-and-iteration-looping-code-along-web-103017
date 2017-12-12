8f1935c5687f6f76b7f1a0bf62e15c430788d7b7
dansbands <dansbands@hotmail.com>
2017-12-11 21:03:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+function printBadges(array) {
+  // const newArray = []
+  for(let i = 0; i < array.length; i++) {
+    console.log(`Welcome ${array[i]}! You are employee #${[i + 1]}.`)
+  }
+  return array
+}
+
+
+function tailsNeverFails() {
+  function tails() {
+    return Math.random() >= 0.5
+  }
+
+  let flips = 0
+  let message = ''
+
+  while (tails()) {
+    flips += 1
+    message = `You got ${flips} tails in a row!`
+  }
+  return message
+}
